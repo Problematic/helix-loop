2f85242a489a2734338647cd1005442e34904b3c
Derek Stobbe <djstobbe@gmail.com>
2015-01-08 23:48:45 -0700

    Let there be code

---
+++ README.md
@@ -0,0 +1,46 @@
+# helix.js
+
+Simple game loop management
+
+```javascript
+var helix = new (require('helix'))({
+    updateInterval: 1000 / 60  // 60 fps, about 16 ms per tick
+});
+
+helix.on('start', function () {
+    // setup that happens once, before the first update tick
+});
+
+helix.on('preUpdate', function () {
+    // stuff that should happen in preparation for update to be called one or more times this frame
+});
+
+helix.on('update', function (dt) {
+    // world simulation, can run multiple times per frame
+});
+
+helix.on('postUpdate', function () {
+    // stuff that should happen after the world is completely simulated this frame
+});
+
+helix.on('render', function (t) {
+    // called once per frame
+});
+
+helix.on('stop', function () {
+    // teardown that happens when the loop is stopped
+});
+
+helix.start();
+// helix.isRunning = true;
+
+helix.pause();
+// helix.isRunning = false;
+
+helix.start();
+// helix.isRunning = true;
+// doesn't fire 'start' event again unless stop is called
+
+helix.stop();
+// helix.isRunning = false;
+```
---
+++ index.js
@@ -0,0 +1,77 @@
+var EventEmitter = require('events').EventEmitter;
+var inherits = require('inherits');
+
+function Helix (options) {
+    EventEmitter.call(this);
+
+    this.options = {
+        updateInterval: 1000 / 60,
+        lagCap: 1000
+    };
+    for (var prop in options) {
+        if (options.hasOwnProperty(prop)) {
+            this.options[prop] = options[prop];
+        }
+    }
+
+    this._firstPass = true;
+    this.isRunning = false;
+    this.tick = 0;
+    this.last = null;
+    this.lag = 0;
+    this.gameAnimationFrame = null;
+}
+
+inherits(Helix, EventEmitter);
+
+Helix.prototype.start = function () {
+    this.isRunning = true;
+    this.last = Date.now();
+
+    if (this._firstPass) {
+        this.emit('start');
+        this._firstPass = false;
+    }
+
+    this.gameAnimationFrame = window.requestAnimationFrame(this._loop.bind(this));
+};
+
+Helix.prototype.stop = function () {
+    this.isRunning = false;
+    this.tick = 0;
+    this.lag = 0;
+    this._firstPass = true;
+    window.cancelAnimationFrame(this.gameAnimationFrame);
+};
+
+Helix.prototype.pause = function () {
+    this.isRunning = false;
+    window.cancelAnimationFrame(this.gameAnimationFrame);
+};
+
+Helix.prototype._loop = function () {
+    this.gameAnimationFrame = window.requestAnimationFrame(this._loop.bind(this));
+
+    var current = Date.now();
+    var delta = current - this.last;
+    this.last = current;
+    this.lag += delta;
+
+    var lagCap = this.options.lagCap;
+    if (lagCap && this.lag > lagCap) {
+        this.lag = lagCap;
+    }
+
+    this.emit('preUpdate');
+    var updateInterval = this.options.updateInterval;
+    while (this.lag >= updateInterval) {
+        this.tick++;
+        this.emit('update', updateInterval / 1000);
+        this.lag -= updateInterval;
+    }
+    this.emit('postUpdate');
+
+    this.emit('render', this.lag / updateInterval);
+};
+
+module.exports = Helix;
